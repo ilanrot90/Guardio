9c8336bc43436a1802083b977790c93aacaa8075
ilanrot <ilanrot90@gmail.com>
2020-07-30 10:58:05 +0300

    Updates

---
+++ precache-manifest.f7d13660073be1dba880ca9bdc57bbe6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91de34422fd5c2480afd837ccbeee3a6",
+    "url": "/Guardio/index.html"
+  },
+  {
+    "revision": "dd0b75efdb206f2b7fae",
+    "url": "/Guardio/static/css/2.74f4c41c.chunk.css"
+  },
+  {
+    "revision": "a208197ec261de57b0d7",
+    "url": "/Guardio/static/css/main.b32364c1.chunk.css"
+  },
+  {
+    "revision": "dd0b75efdb206f2b7fae",
+    "url": "/Guardio/static/js/2.68ebfb9a.chunk.js"
+  },
+  {
+    "revision": "a632ac87aa4e3b31bc8ffa65214162a7",
+    "url": "/Guardio/static/js/2.68ebfb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252a056fc42436503869",
+    "url": "/Guardio/static/js/3.c646327c.chunk.js"
+  },
+  {
+    "revision": "1a415f274bf11686f174",
+    "url": "/Guardio/static/js/4.d0c91084.chunk.js"
+  },
+  {
+    "revision": "a208197ec261de57b0d7",
+    "url": "/Guardio/static/js/main.b174716f.chunk.js"
+  },
+  {
+    "revision": "83152895a7ad5757a815",
+    "url": "/Guardio/static/js/runtime-main.d50927fc.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/Guardio/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/Guardio/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Guardio/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Guardio/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "a3e2306ba7312e77fe5875a4131ec39e",
+    "url": "/Guardio/static/media/icon.a3e2306b.svg"
+  }
+]);
